3ab48c3f8a1ed5e8f37ff6f837ae7457099b3197
phantom <phantomdesign9@gmail.com>
2019-02-19 12:09:34 -0600

    working game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,109 +1,142 @@
-// requirements
+// 0. set a game default value on load & a reset function for a new game
 // 1. a random number is presented to the user a "target number"
 // 2. the randomNumber range is from 19-120
 // 3. there are four crystals for the user to click
 // 4. each crystal is assigned a random value
 // 5. the randomValue range is 1-12
-// 6. each clicked value is added to create a user's score
-// 7. if the user score equals the target number, then increment a win counter and reset the game variables
+// 6. each clicked value is added to create a user's score (contain win scenario in each click)
+// 7. if the user score equals the target number, then increment a win counter and load new game variables
 // 8. if the user score is less than the target number, additional clicks are allowed
-// 9. if the user score is greaterthan the target number, increment the loss counter and reset the game variables
-
-// Here we define a few gloable variables
-// =========================================
-
-var wins = 0;
-var losses = 0;
-var crystalCount = [];
-var userScore = 0;
-var goldValue = 0;
-var redValue = 0;
-var blueValue = 0;
-var whiteValue = 0;
-var targetNumber = 0;
-
-console.log(wins)
-console.log(losses)
-console.log(userScore)
-console.log("gold " + goldValue)
-console.log("red " + redValue)
-console.log("blue " + blueValue)
-console.log("whiteValue " + whiteValue)
+// 9. if the user score is greaterthan the target number, increment the loss counter and load new game variables
 
 // Here we are staring the game when the document has loaded
-// ========================================================================================
+// ==========================================================================================
 
 $(document).ready(function() {
 
-  // Here we'll create a newGame reset function after wins and losses
+  // Here we set the default game values after the doc loads
+  // ========================================================================================
+      // set initial game variables ==>
+      var wins = 0;
+      var losses = 0;
+      var userScore = 0;
+
+      // display initial game variables in game space ==> 
+      $( "#countWins" ).text(wins);
+      $( "#countLosses" ).text(losses);
+      $( "#userScore" ).text(userScore);
+
+      // create target number and display on game scoreboard ==> 
+      var min=19;  
+      var max=120;  
+      var targetNumber = Math.floor(Math.random() * (+max - +min)) + +min; //googleFoo, controlled random integer
+      $( "#targetNumber" ).text(targetNumber); 
+
+      // assign a random value to each crystal and not display it ==> 
+      var goldValue = Math.floor(Math.random() * 12 + 1); //using 12 so I can incude 12 as a possible value
+      var redValue = Math.floor(Math.random() * 12 + 1);
+      var blueValue = Math.floor(Math.random() * 12 + 1);
+      var whiteValue = Math.floor(Math.random() * 12 + 1);
+
+      // qc our initial game values ==>
+      console.log("The Target Number is  " + targetNumber);
+      console.log("The Crystal Values (gold, red, blue, white) is  " + goldValue + ", " + redValue + ", " + blueValue +", " + whiteValue);
+
+  // Here we'll create functions to be called during game play
+  // ========================================================================================
+      
+      // reset the game target, score, and click values ==>
+
+      function reset() {
+        min=19;  
+        max=120;  
+        targetNumber = Math.floor(Math.random() * (+max - +min)) + +min; //create new targetNumber
+        $( "#targetNumber" ).text(targetNumber); //display new targetNumber
+        goldValue = Math.floor(Math.random() * 12 + 1); //establish new crystal values
+        redValue = Math.floor(Math.random() * 12 + 1);
+        blueValue = Math.floor(Math.random() * 12 + 1);
+        whiteValue = Math.floor(Math.random() * 12 + 1);    
+        userScore = 0;
+        $ ( "#userScore" ).text(userScore); //display reset userScore
+        console.log(goldValue + " " + redValue + " " + blueValue + " " + whiteValue)
+        console.log("new user score should be zero but " + userScore)
+        }
+
+        // qa the mystery crystal values ==>
+        console.log("Gold click will be " + goldValue)
+        console.log("Red click will be " + redValue)
+        console.log("Blue click will be " + blueValue)
+        console.log("White click will be " + whiteValue)
+
+    // winner event that resets the game ==>
+
+    function winner() {
+      wins++;
+      $( "#countWins" ).text(wins);
+      reset ()
+    }
+
+    // loser event that resets the game ==>
+
+    function loser() {
+      losses++;
+      $( "#countLosses" ).text(losses);
+      reset ()
+    }
+
+  // Here we'll set the game space for click events
   // ========================================================================================
 
-    function newGame () {
-    userScore = 0;
-    console.log(userScore);
- 
-  // Here we'll generate a target number for the current game (had help from google here)
-  // ========================================================================================
+    // when the user clicks the GOLD crystal ==>
 
-    var min=19;
-    var max=120;
-    targetNumber = Math.floor(Math.random() * (+max - +min)) + +min;
-    console.log("Random number generated is  " + targetNumber);
-    $( "#targetNumber" ).html(targetNumber);
+    $("#gold").click(function () {
+      userScore = (goldValue + userScore);  //add the gold value to the user's score
+      $( "#userScore" ).text(userScore);    //display post-click userScore
+      if (userScore == targetNumber) {     
+        winner ()
+      }
+      else if (userScore > targetNumber) {
+        loser ()
+      }
+    });
 
-// Here we'll generate a random Crystal Value for the current game
-// ========================================================================================
-    
-    var gMin=1;
-    var gMax=12;
-    goldValue = Math.floor(Math.random() * (+gMax - +gMin)) + +gMin;
-    console.log("Gold click value is  " + goldValue);
+    // when the user clicks the RED crystal ==>
 
-    var rMin=1;
-    var rMax=12;
-    redValue = Math.floor(Math.random() * (+rMax - +rMin)) + +rMin;
-    console.log("Red click value is  " + redValue);
+    $("#red").click(function () {
+      userScore = (redValue + userScore);
+      $( "#userScore" ).text(userScore);
+      if (userScore == targetNumber) {     
+        winner ()
+      }
+      else if (userScore > targetNumber) {
+        loser ()
+      }
+    });
 
-    var bMin=1;
-    var bMax=12;
-    blueValue = Math.floor(Math.random() * (+bMax - +bMin)) + +bMin;
-    console.log("Blue click value is  " + blueValue);
+    // when the user clicks the BLUEcrystal ==>
 
-    var wMin=1;
-    var wMax=12;
-    whiteValue = Math.floor(Math.random() * (+wMax - +wMin)) + +wMin;
-    console.log("White click value is  " + whiteValue);
+    $("#blue").click(function () {
+      userScore = (blueValue + userScore);
+      $ ( "#userScore" ).text(userScore);
+      if (userScore == targetNumber) {     
+        winner ()
+      }
+      else if (userScore > targetNumber) {
+        loser ()
+      }
+    });
 
-// Here we'll use the crystal clicks to accumulate a user's score
-// ========================================================================================
+    // when the user clicks the WHITE crystal ==>
 
-  $("#gold").click(function () {
-    console.log("white click score " + (userScore + goldValue))
-  });
-
-  $("#red").click(function () {
-    console.log("Red click score " + (userScore + redValue))
-  });
-
-  $("#blue").click(function () {
-    console.log("blue click score " + (userScore + blueValue))
-  });
-
-  $("#white").click(function () {
-    console.log("white click score " + (userScore + whiteValue))
-  });
-
-  console.log(wins)
-  console.log(losses)
-  console.log(userScore)
-  console.log("gold " + goldValue)
-  console.log("red " + redValue)
-  console.log("blue " + blueValue)
-  console.log("whiteValue " + whiteValue)
-
-}
-// call reset
-// ========================================================================================
-  newGame();
+    $("#white").click(function () {
+      userScore = (whiteValue + userScore);
+      $( "#userScore" ).text(userScore);
+      if (userScore == targetNumber) {     
+        winner ()
+      }
+      else if (userScore > targetNumber) {
+        loser ()
+      }
+    });
 });
 
